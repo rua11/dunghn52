01e4f4d0a72cf55596e9f3e40f52f759cca804fc
Dũng Anh Hoang <ngocdungnb06@gmail.com>
2022-08-01 16:30:00 +0700

    update router

--- main.py
+++ main.py
@@ -1,6 +1,10 @@
 from fastapi import FastAPI
 from core.commons import base_model 
 from core.commons.ibase_service import engine
+from modules.users.api.v1 import work_unit_api
 
 base_model.Base.metadata.create_all(bind=engine)
 app = FastAPI()
+
+
+app.include_router(work_unit_api.router)
---
+++ modules/users/router.py
@@ -0,0 +1,8 @@
+
+from fastapi import APIRouter
+from modules.users.api.v1 import work_unit_api
+
+prefix_url = '/work_unit' 
+
+
+routes = APIRouter() 
