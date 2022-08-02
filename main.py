1e60dc50917037e8abb759c926f457b1c4503f31
Dũng Anh Hoang <ngocdungnb06@gmail.com>
2022-08-02 09:32:30 +0700

    clear

--- main.py
+++ main.py
@@ -2,10 +2,8 @@ from fastapi import FastAPI
 from core.commons import base_model 
 from core.commons.ibase_service import engine
 from modules import router_builder
-# from modules.users.api.v1 import work_unit_api
 
 base_model.Base.metadata.create_all(bind=engine)
 app = FastAPI()
 
 router_builder(app)
-# app.include_router(work_unit_api.router)
