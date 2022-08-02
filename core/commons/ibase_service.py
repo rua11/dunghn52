89ad3a97f40bb13c703c3ced867549aa810f5570
Dũng Anh Hoang <ngocdungnb06@gmail.com>
2022-08-02 13:48:16 +0700

    cấu hình kết nối fb

--- core/commons/ibase_service.py
+++ core/commons/ibase_service.py
@@ -5,16 +5,16 @@ from sqlalchemy.orm import sessionmaker
 from configs.dev_config import DATABASE
 T= TypeVar("T")
 
-SQLALCHEMY_DATABASE_URL = "postgresql://postgres:123456@localhost/dun"
-engine = create_engine(SQLALCHEMY_DATABASE_URL)
-SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
-# SQLALCHEMY_DATABASE_URL = "postgresql://%s:%s@%s/%s"
+# SQLALCHEMY_DATABASE_URL = "postgresql://postgres:123456@localhost/dun"
+# engine = create_engine(SQLALCHEMY_DATABASE_URL)
+# SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
+SQLALCHEMY_DATABASE_URL = "postgresql://%s:%s@%s/%s"
 
 
         
 class IBaseService:
     def __init__(self):
-        self.db = SessionLocal()
+        self.db = PgService().SessionLocal()
         try:
             self.db
         finally:
@@ -35,15 +35,15 @@ class IBaseService:
             raise(ex)
         
     
-# class PgService:
-#     def __init__(self):
+class PgService:
+    def __init__(self):
        
-#         self.DB_URL = SQLALCHEMY_DATABASE_URL % (DATABASE['default']['user'], DATABASE['default']['password'],
-#                                             DATABASE['default']['host'], DATABASE['default']['db_name'])
-#         self.engine = create_engine(self.DB_URL)
-#         self.SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=self.engine)
-#     def __call__(self):
-#             pass
+        self.DB_URL = SQLALCHEMY_DATABASE_URL % (DATABASE['default']['user'], DATABASE['default']['password'],
+                                            DATABASE['default']['host'], DATABASE['default']['db_name'])
+        self.engine = create_engine(self.DB_URL)
+        self.SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=self.engine)
+    def __call__(self):
+            pass
                        
 
 # def get_db():
--- main.py
+++ main.py
@@ -1,9 +1,9 @@
 from fastapi import FastAPI
-from core.commons import base_model 
-from core.commons.ibase_service import engine
+# from core.commons import base_model 
+# from core.commons.ibase_service import engine
 from modules import router_builder
 
-base_model.Base.metadata.create_all(bind=engine)
+# base_model.Base.metadata.create_all(bind=engine)
 app = FastAPI()
 
 router_builder(app)
